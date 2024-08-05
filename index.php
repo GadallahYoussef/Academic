2896f9e305c1394934d06987c576f4323f9aceaa
youssef mostafa <yomu611@gmail.com>
2024-08-05 15:41:14 +0300

    sdf

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ session_start();
 include('connection.php');
 include('function.php');
 header('Content-Type: application/json');
-header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Origin: http://localhost:5174");
 header("Access-Control-Allow-Methods: POST");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
@@ -16,13 +16,13 @@ if ($continue) {
     }
 }
 if ($authenticated) {
-    $schedule = $conn->prepare("SELECT id, day, start, end from schedule WHERE grade = ? and section = ?");
+    $schedule = $conn->prepare("SELECT day, start, end from schedule WHERE grade = ? and section = ? ORDER BY id");
     $schedule->bind_param('is', $_SESSION['grade'], $_SESSION['section']);
     $schedule->execute();
     $schedule->store_result();
     if ($schedule->num_rows > 0) {
         $schedule_day = [];
-        $schedule->bind_result($id, $day, $start, $end);
+        $schedule->bind_result($day, $start, $end);
         while ($schedule->fetch()) {
             $schedule_day[] = array($day, $start, $end);
         }
