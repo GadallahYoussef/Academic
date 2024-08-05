dae04a408d118c9caa5b5aea5d7496f572c71a9a
youssef mostafa <yomu611@gmail.com>
2024-08-05 16:42:24 +0300

    klkjlj

--- retrieve_materials.php
+++ retrieve_materials.php
@@ -18,23 +18,19 @@ if ($continue and $_SESSION['status'] === 'active') {
     $authenticated = true;
 }
 if ($authenticated) {
-    $material = $conn->prepare("SELECT id, type, description, url FROM materials WHERE (grade = ? and section = ?) or (grade = ? and section = 'all')");
+    $material = $conn->prepare("SELECT type, description, url FROM materials WHERE (grade = ? and section = ?) or (grade = ? and section = 'all') ORDER BY id DESC");
     $material->bind_param('isi', $_SESSION['grade'], $_SESSION['section'], $_SESSION['grade']);
     if ($material->execute()) {
         $material->store_result();
         if ($material->num_rows > 0) {
             $student_material = [];
-            $material->bind_result($id, $type, $description, $url);
+            $material->bind_result($type, $description, $url);
             while ($material->fetch()) {
-                $student_material[$id] = [];
-                $student_material[$id]['type'] = $type;
                 if (is_arabic($description)) {
                     $rtl_description = "\u{202B}" . $description . "\u{202C}";
-                    $student_material[$id]['caption'] = $rtl_description;
-                } else {
-                    $student_material[$id]['caption'] = $description;
+                    $description = $rtl_description;
                 }
-                $student_material[$id]['path'] = $url;
+                $student_material[] = array('type' => $type, 'caption' => $description, 'path' => $url);
             }
             echo json_encode(['status' => 'OK', 'authenticated' => $authenticated, 'found' => true, 'matrials' => $student_material], JSON_UNESCAPED_UNICODE);
             $material->close();
