17cedcd963933cf5fc0b83ae86b80304c12b1ef7
youssef mostafa <yomu611@gmail.com>
2024-08-09 15:25:06 +0300

    ya bdaaany

---
+++ admin/current_classes.php
@@ -0,0 +1,10 @@
+<?php
+session_start();
+include('../connection.php');
+include('admin_connection.php');
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Access-Control-Allow-Credentials: true");
+session_get_cookie_params();
